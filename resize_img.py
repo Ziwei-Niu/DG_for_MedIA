ecc2024b1651a1ada625bcb121d949899592343c
Ziwei-Niu <nzw@zju.edu.cn>
2025-01-06 16:00:29 +0800

    change style

--- resize_img.py
+++ resize_img.py
@@ -3,7 +3,7 @@ import os
 
 # 设置文件夹路径和目标宽度
 folder_path = 'images/'  # 替换为你文件夹的路径
-target_width = 1000  # 你希望的统一宽度
+target_width = 150  # 你希望的统一宽度
 
 # 获取文件夹中的所有图片文件
 for filename in os.listdir(folder_path):
